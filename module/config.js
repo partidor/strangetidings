05424c1c2b2110c75196283c02cf142399d45cf1
lakitu <lakitu@cloud.utopia.net>
2020-12-13 08:37:43 -0500

    Added more weapon localization and options

--- module/config.js
+++ module/config.js
@@ -10,6 +10,61 @@ strangetidings.attackTypes = {
     weird: "stlang.stats.weird"
 }
 
+strangetidings.materials = {
+    wood: "stlang.item.material.wood",
+    steel: "stlang.item.material.steel",
+    leather: "stlang.item.material.leather",
+    vestment: "stlang.item.material.vestment",
+    bronze: "stlang.item.material.bronze",
+    iron: "stlang.item.material.iron",
+    silver: "stlang.item.material.silver",
+    crystal: "stlang.item.material.crystal",
+    siderite: "stlang.item.material.siderite"
+}
+
+strangetidings.customization = {
+	reach: "stlang.item.customization.reach", 
+	thrown: "stlang.item.customization.thrown", 
+	long_: "stlang.item.customization.long", 
+	serrated: "stlang.item.customization.serrated", 
+	stiletto: "stlang.item.customization.stiletto", 
+	fluted: "stlang.item.customization.fluted", 
+	elementalsheath: "stlang.item.customization.elementalsheath", 
+	complex: "stlang.item.customization.complex", 
+	wrist: "stlang.item.customization.wrist", 
+	siege: "stlang.item.customization.siege", 
+	longfirearm: "stlang.item.customization.longfirearm", 
+	shortfirearm: "stlang.item.customization.shortfirearm", 
+	poison: "stlang.item.customization.poison"
+}
+
+strangetidings.damageTypes = {
+    piercing: "stlang.misc.damagetype.piercing",
+    slashing: "stlang.misc.damagetype.slashing",
+    blunt: "stlang.misc.damagetype.blunt",
+    fire: "stlang.misc.damagetype.fire",
+    lightning: "stlang.misc.damagetype.lightning",
+    ice: "stlang.misc.damagetype.ice",
+    thunder: "stlang.misc.damagetype.thunder",
+    acid: "stlang.misc.damagetype.acid",
+    poison: "stlang.misc.damagetype.poison",
+    radiant: "stlang.misc.damagetype.radiant",
+    necrotic: "stlang.misc.damagetype.necrotic",
+    psychic: "stlang.misc.damagetype.psychic",
+    celestial: "stlang.misc.damagetype.celestial",
+    occult: "stlang.misc.damagetype.occult",
+    ith: "stlang.misc.damagetype.ith",
+    cer: "stlang.misc.damagetype.cer",
+    force: "stlang.misc.damagetype.force",
+}
+
+strangetidings.sizes = {
+    small: "stlang.item.size.small",
+    medium: "stlang.item.size.medium",
+    large: "stlang.item.size.large",
+    great: "stlang.item.size.great"
+}
+
 strangetidings.weapon = {
     attackroll: "stlang.item.weapon.attackroll",
     attackbonus: "stlang.item.weapon.attackbonus",
@@ -17,5 +72,6 @@ strangetidings.weapon = {
     damagebonus: "stlang.item.weapon.damagebonus",
     damagetype: "stlang.item.weapon.damagetype",
     customization: "stlang.item.weapon.customization",
+    material: "stlang.item.weapon.material",
     size: "stlang.item.weapon.size"
 }
